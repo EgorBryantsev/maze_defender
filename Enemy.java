da6245e12c589c64a36f6bd903155729697854ff
Egor Bryantsev <egor.bryantsev1@gmail.com>
2024-10-24 02:10:31 +0200

    moved pathfinding

--- Enemy.java
+++ Enemy.java
@@ -4,8 +4,6 @@ public class Enemy {
     private int x, y; //location
     private int hp; //health
     private double speed;
-    private List<Node> path; //for A*
-    private int currentPathIndex;
     private int points;
 
     public Enemy(int startX, int startY, int hp, double speed, int points) {
@@ -14,19 +12,9 @@ public class Enemy {
         this.hp = hp;
         this.speed = speed;
         this.points = points;
-        this.currentPathIndex = 0;
-    }
-
-    public void setPath(List<Node> path) {
-        this.path = path;
     }
 
     public void move() {
-        if (path == null  || currentPathIndex >= path.size()) {
-            return;
-        }
-
-        Node targetNode =
     }
 
     public void takeDamage() {
