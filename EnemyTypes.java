bef2c086f694a4669d47ac5bff6a843c4df80dac
Egor Bryantsev <egor.bryantsev1@gmail.com>
2024-10-27 19:25:32 +0100

    made enemies scale with window size

--- EnemyTypes.java
+++ EnemyTypes.java
@@ -1,10 +1,10 @@
 import java.awt.Color;
 
-public record EnemyTypes(double speed, int hp, int points, Color color, int size) {
+public record EnemyTypes(double speed, int hp, int points, Color color, double sizeRatio) {
 
-    public static final EnemyTypes BASIC = new EnemyTypes(1.0, 100, 10, Color.RED, 20);
-    public static final EnemyTypes FAST = new EnemyTypes(2.0, 50, 15, Color.BLUE, 15);
-    public static final EnemyTypes TANK = new EnemyTypes(0.5, 300, 30, Color.DARK_GRAY, 30);
-    public static final EnemyTypes FLYER = new EnemyTypes(3.0, 75, 20, Color.MAGENTA, 18);
+    public static final EnemyTypes BASIC = new EnemyTypes(2.0, 100, 10, Color.RED, 0.8);
+    public static final EnemyTypes FAST = new EnemyTypes(3.5, 50, 15, Color.ORANGE, 0.6);
+    public static final EnemyTypes TANK = new EnemyTypes(1.5, 300, 30, Color.YELLOW, 1.0);
+    public static final EnemyTypes FLYER = new EnemyTypes(4.0, 75, 20, Color.GREEN, 0.4);
 
 }
